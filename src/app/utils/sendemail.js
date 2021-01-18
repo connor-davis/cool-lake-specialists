ae0cc7ed0046d11752aad4626d0f2886341c5780
Connor Davis <19422@kloofhigh.co.za>
2021-01-18 13:40:47 +0200

    Hopefully this contact system works now

--- src/app/utils/sendemail.js
+++ src/app/utils/sendemail.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export let sendEmail = async ({ subject, from, user_name, message }) => {
-    let response = await axios.post("https://powerful-dawn-57349.herokuapp.com/mail", { subject, from, to: "19422@kloofhigh.co.za", user_name, message, smtp: { host: "mail.coollake.co.za", port: 25 } }, {});
-
+    let response = await axios.post("https://powerful-dawn-57349.herokuapp.com/mail", { subject, from, to: "contact@coollake.co.za", user_name, message, smtp: { host: "mail.coollake.co.za", port: 25 } }, {});
     return response.status;
 }
