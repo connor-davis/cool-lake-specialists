fa9eda04138c5a8345b9bec27f545c42e81363d8
Connor Davis <19422@kloofhigh.co.za>
2021-02-10 20:24:42 +0200

    update

--- src/app/utils/sendemail.js
+++ src/app/utils/sendemail.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export let sendEmail = async ({ subject, from, user_name, message }) => {
-  let response = await axios.post("https://powerful-dawn-57349.herokuapp.com/mail", { subject, from, to: "connor.dvs03@gmail.com", user_name, message}, {});
+  let response = await axios.post("https://powerful-dawn-57349.herokuapp.com/mail", { subject, from, to: "contact@coollake.co.za", user_name, message}, {});
   // let response = await axios.post("http://localhost:8080/mail", { subject, from, to: "connor.dvs03@gmail.com", user_name, message}, {});
 
   return response.status;
